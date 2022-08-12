01210077e5012a18ecebbce7cdb42f2646b4a529
Dilshod Fayzullayev <darkprohub-uz@yandex.com>
2022-08-12 13:26:01 +0500

    create pc market app in spring security

---
+++ src/main/java/uz/pdp/springsecuritypcmarket/SpringSecurityPcMarketApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.springsecuritypcmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPcMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPcMarketApplication.class, args);
+    }
+
+}
